89d4398ef7e27556a5e4988c3921cf5439744b86
Michael Bukachi <michaelbukachi@gmail.com>
2019-09-29 23:05:49 +0300

    refactor: Remove GlobalScope

--- app/src/main/java/ke/co/appslab/androidpagingwithcoroutines/networking/ApiService.kt
+++ app/src/main/java/ke/co/appslab/androidpagingwithcoroutines/networking/ApiService.kt
@@ -1,7 +1,6 @@
 package ke.co.appslab.androidpagingwithcoroutines.networking
 
 import ke.co.appslab.androidpagingwithcoroutines.models.RedditApiResponse
-import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,9 +8,9 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("/r/aww/hot.json")
-    fun fetchPosts(
+    suspend fun fetchPosts(
         @Query("limit") loadSize: Int = 30,
         @Query("after") after: String? = null,
         @Query("before") before: String? = null
-    ) : Deferred<Response<RedditApiResponse>>
+    ): Response<RedditApiResponse>
 }
--- app/src/main/java/ke/co/appslab/androidpagingwithcoroutines/repositories/PostsDataSource.kt
+++ app/src/main/java/ke/co/appslab/androidpagingwithcoroutines/repositories/PostsDataSource.kt
@@ -1,22 +1,22 @@
 package ke.co.appslab.androidpagingwithcoroutines.repositories
 
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import androidx.paging.PageKeyedDataSource
 import ke.co.appslab.androidpagingwithcoroutines.models.RedditPost
 import ke.co.appslab.androidpagingwithcoroutines.networking.ApiClient
 import ke.co.appslab.androidpagingwithcoroutines.networking.ApiService
-import ke.co.appslab.androidpagingwithcoroutines.utils.safeApiCall
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
-class PostsDataSource : PageKeyedDataSource<String,RedditPost>() {
+class PostsDataSource(private val scope: CoroutineScope) :
+    PageKeyedDataSource<String, RedditPost>() {
     private val apiService = ApiClient.getClient().create(ApiService::class.java)
 
     override fun loadInitial(params: LoadInitialParams<String>, callback: LoadInitialCallback<String, RedditPost>) {
-        GlobalScope.launch {
+        scope.launch {
             try {
-                val response = apiService.fetchPosts(loadSize = params.requestedLoadSize).await()
+                val response = apiService.fetchPosts(loadSize = params.requestedLoadSize)
                 when{
                     response.isSuccessful -> {
                         val listing = response.body()?.data
@@ -34,9 +34,10 @@ class PostsDataSource : PageKeyedDataSource<String,RedditPost>() {
     }
 
     override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<String, RedditPost>) {
-        GlobalScope.launch {
+        scope.launch {
             try {
-                val response = apiService.fetchPosts(loadSize = params.requestedLoadSize,after = params.key).await()
+                val response =
+                    apiService.fetchPosts(loadSize = params.requestedLoadSize, after = params.key)
                 when{
                     response.isSuccessful -> {
                         val listing = response.body()?.data
@@ -53,9 +54,10 @@ class PostsDataSource : PageKeyedDataSource<String,RedditPost>() {
     }
 
     override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<String, RedditPost>) {
-        GlobalScope.launch {
+        scope.launch {
             try {
-                val response = apiService.fetchPosts(loadSize = params.requestedLoadSize,  before = params.key).await()
+                val response =
+                    apiService.fetchPosts(loadSize = params.requestedLoadSize, before = params.key)
                 when{
                     response.isSuccessful -> {
                         val listing = response.body()?.data
@@ -69,6 +71,11 @@ class PostsDataSource : PageKeyedDataSource<String,RedditPost>() {
             }
         }
 
+    }
+
+    override fun invalidate() {
+        super.invalidate()
+        scope.cancel()
     }
 
 }
